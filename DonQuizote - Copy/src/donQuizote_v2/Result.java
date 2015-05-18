427c56b89c07e1d7e956b96b738775fce402f809
Michael Skelly <skellystudios@gmail.com>
2015-05-18 18:05:25 +0100

    Remove junk folder

--- DonQuizote - Copy/src/donQuizote_v2/Result.java
+++
@@ -1,7 +0,0 @@
-package donQuizote_v2;
-
-public class Result {
-	
-	//public Result 
-
-}
--- DonQuizote - Copy/src/donQuizote_v2/Writable.java
+++
@@ -1,7 +0,0 @@
-package donQuizote_v2;
-
-public interface Writable {
-	
-	public void updateText(String s);
-
-}
